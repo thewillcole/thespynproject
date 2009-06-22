d63e9ad193b94024695741011246d15ff1143f30
wbcole <wbcole@42149c8e-5733-11de-b850-a5b992301b95>
2009-06-22 02:00:26 +0000

    The BOD works, but doesn't pass the rowcount from ScanMe to Note_Edit. However, the DB does have a field for the rowcount and is temporarily storing the value 0.

--- trunk/spyn/src/com/spyn/ScanMe.java
+++ trunk/spyn/src/com/spyn/ScanMe.java
@@ -19,15 +19,19 @@
 
 package com.spyn;
 
-import java.io.File;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ScanMe extends Activity {
+	
+	public final static String INTENT_BITMAP="bitmap";
+	public final static String INTENT_AVGROW="avgrow";
 	
 	public final static String ACTION_STORE = "store";
 	public final static String ACTION_EXTRACT = "extract";
@@ -37,17 +41,12 @@ public class ScanMe extends Activity {
 	public final static String SCAN_LOCATION = "scan_location";
 	
 	public String myAction;
-	public String myPath;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Intent intent = getIntent();
-		myPath = intent.getStringExtra(ScanMe.IMAGE_PATH);
 		myAction = intent.getAction();
-		
-		//get the image however you want, here it is as a file
-		File imageFile = new File(myPath);
 		
 		if (myAction.equals(ScanMe.ACTION_STORE)) {
 			// scan for a new memory
@@ -63,14 +62,12 @@ public class ScanMe extends Activity {
 	
 	// scan for a new memory
 	public void callStoreScan() {
-		int[] location; // X,Y row count
+		Intent intent = getIntent();
+		Bitmap bitmap = (Bitmap) intent.getExtras().get(ScanMe.INTENT_BITMAP);
+		int avgrow = countStitches(bitmap);
 		
-		// XXX your code here
-		
-		
-		// how to return data to the caller
 		Intent returnIntent = new Intent();
-		/*returnIntent.putExtra(ScanMe.SCAN_LOCATION, location);*/
+		returnIntent.putExtra(INTENT_AVGROW, avgrow);
 		setResult(RESULT_OK, returnIntent);
 		
 		//this is a good, cheap way to exit the ScanMe activity
@@ -97,6 +94,107 @@ public class ScanMe extends Activity {
 		onKeyDown(KeyEvent.KEYCODE_BACK, new KeyEvent(KeyEvent.KEYCODE_BACK, KeyEvent.ACTION_DOWN));
 	}
 	
+	
+	//---------------------
+	public int countStitches(Bitmap bitmap) {
+		//URL imgURL;
+		//Bitmap bmImg = null;
+		ImageView image = new ImageView(this); // display Object build into Android
+		setContentView(image); // displays ImageView on screen
+		return processImg(bitmap);
+	}
+
+	public int processImg(Bitmap mybit) {
+		int width = mybit.getWidth();
+		int height = mybit.getHeight();
+		int[] pixels = new int[width*height];
+		mybit.getPixels(pixels,0,width,0,0,width, height);
+		//byte[] picData = mybit.getData();
+
+		if(width==-1 || height==-1){
+			// the image has not loaded.
+			mybit = (Bitmap)null;
+		}
+		final int STHRESH = 10; // Threshold for stitch size ** this should change 
+		final int MAXROWS = width;
+		final int MAXCOLS = height;
+
+		int[][] matrix = new int[MAXROWS][MAXCOLS];
+		System.out.println("MAXROWS " + MAXROWS);
+		System.out.println("MAXCOLS " + MAXCOLS);
+		int count = 0;
+		int countoff = 0;
+		int edgeBool = 0;
+		float avg1 =1;
+		float avg2 =1;
+		float avg3 =1;
+		float avg4 =1;
+		float avg5 =1;
+		float avg6 =1;
+		float avg7 =1;
+		float avg8 =1;
+		float avg9 =1;
+		for (int row=1; row<MAXROWS-1; row++) {
+			for (int col=1; col<MAXCOLS-1; col++) {
+				avg1 = (((mybit.getPixel(row+1,col+1) >> 16) & 0xFF) + ((mybit.getPixel(row+1,col+1) >> 8) & 0xFF)+ ((mybit.getPixel(row+1,col+1)) & 0xFF))/3;
+				avg2 = (((mybit.getPixel(row+1,col-1) >> 16) & 0xFF) + ((mybit.getPixel(row+1,col-1) >> 8) & 0xFF)+ ((mybit.getPixel(row+1,col-1)) & 0xFF))/3;
+				avg3 = (((mybit.getPixel(row+1,col) >> 16) & 0xFF) + ((mybit.getPixel(row+1,col) >> 8) & 0xFF)+ ((mybit.getPixel(row+1,col)) & 0xFF))/3;
+
+				avg4 = (((mybit.getPixel(row,col+1) >> 16) & 0xFF) + ((mybit.getPixel(row,col+1) >> 8) & 0xFF)+ ((mybit.getPixel(row,col+1)) & 0xFF))/3;
+				avg5 = (((mybit.getPixel(row,col-1) >> 16) & 0xFF) + ((mybit.getPixel(row,col-1) >> 8) & 0xFF)+ ((mybit.getPixel(row,col-1)) & 0xFF))/3;;
+				avg6 = (((mybit.getPixel(row,col) >> 16) & 0xFF) + ((mybit.getPixel(row,col) >> 8) & 0xFF)+ ((mybit.getPixel(row,col)) & 0xFF))/3;
+
+				avg7 = (((mybit.getPixel(row-1,col+1) >> 16) & 0xFF) + ((mybit.getPixel(row-1,col+1) >> 8) & 0xFF)+ ((mybit.getPixel(row-1,col+1)) & 0xFF))/3;
+				avg8 = (((mybit.getPixel(row-1,col-1) >> 16) & 0xFF) + ((mybit.getPixel(row-1,col-1) >> 8) & 0xFF)+ ((mybit.getPixel(row-1,col-1)) & 0xFF))/3;
+				avg9 = (((mybit.getPixel(row-1,col) >> 16) & 0xFF) + ((mybit.getPixel(row-1,col) >> 8) & 0xFF)+ ((mybit.getPixel(row-1,col)) & 0xFF))/3;
+
+				float avg = avg5;
+				if ((((avg1+avg2+avg3)-(avg7+avg8+avg9))>1)||((avg3+avg6+avg9)-(avg1+avg4+avg7))>1)
+				{
+					matrix[row][col] = 1;
+				} else {
+					matrix[row][col] = 0;
+				}
+			}  // for col
+			// for row
+
+			float avgrow = 0;
+			float totalrow = 0;
+			for (row=1; row<MAXROWS-1; row++) {
+				for (int col=1; col<MAXCOLS-1; col++) {
+					if ((matrix[row][col]==1)&&(countoff<STHRESH)) {
+						if ((edgeBool == 0)){//&&(countoff>0)) {
+							count++;
+							edgeBool = 1;
+							countoff = 0;
+						}
+						countoff++;
+						//System.out.println("Number of rows: " + count +", " +countoff);
+					} else {
+						if (edgeBool == 1) {
+							edgeBool = 0;
+						}
+					}
+				}
+				totalrow += count;
+				count=0;
+				countoff=0;
+			}
+			avgrow = totalrow/(MAXROWS-1);
+			System.out.println("Blue image num of rows: " + avgrow +", " +countoff);
+			TextView tv = new TextView(this);
+			tv.setText("Dimensions: "+width+" x "+height+"\n Test data:"+" "+((((mybit.getPixel(20,20) >> 16) & 0xFF) + ((mybit.getPixel(20,20) >> 8) & 0xFF)+ ((mybit.getPixel(20,20)) & 0xFF))/3)+".\n Total number of rows: " + totalrow+ " Average num: "+avgrow +", " +countoff+" "+matrix[20][20]+" "+matrix[21][21]+" "+matrix[22][22]+" "+matrix[23][23]+" "+avg5+" "+avg6+" "+avg7+" "+avg8+" "+avg9);
+			setContentView(tv);
+			// return row count (this takes int ceiling of the float)
+			//Toast.makeText(this, "ROWCOUNTIZZLE: " + avgrow, Toast.LENGTH_LONG * 10).show();
+			Float avgRow = new Float(avgrow);
+			return avgRow.intValue() + 1;
+
+		}
+		// error: the for-loop was never executed
+		return -1;
+	}
+	//---------------------
 
 	
 }
